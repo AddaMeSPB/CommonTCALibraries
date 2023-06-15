ce53c58d55d008f3b22dc5c0152b8ee9eaf005a3
saroar <saroar9@gmail.com>
2023-06-15 09:34:48 +0300

    Update Image Compress

--- Sources/FoundationExtension/Image+Compress.swift
+++ Sources/FoundationExtension/Image+Compress.swift
@@ -19,83 +19,118 @@ import os.log
 /// }
 
 #if os(iOS)
-  import UIKit
+import UIKit
 #elseif os(OSX)
-  import AppKit
-  import Cocoa
+import AppKit
+import Cocoa
 
-  typealias UIImage = NSImage
-  extension NSImage {
+typealias UIImage = NSImage
+extension NSImage {
     var cgImage: CGImage? {
-      var proposedRect = CGRect(origin: .zero, size: size)
+        var proposedRect = CGRect(origin: .zero, size: size)
 
-      return cgImage(
-        forProposedRect: &proposedRect,
-        context: nil,
-        hints: nil)
+        return cgImage(
+            forProposedRect: &proposedRect,
+            context: nil,
+            hints: nil)
     }
 
     convenience init?(named name: String) {
-      self.init(named: Name(name))
+        self.init(named: Name(name))
     }
-  }
+}
 
 #endif
 
+enum ImageCompressionError: Error {
+    case compressionFailed
+    case invalidImageData
+}
+
 public enum CompressionQuality: CGFloat {
-  case lowest = 0
-  case low = 0.25
-  case medium = 0.5
-  case high = 0.75
-  case highest = 1
+    case lowest = 0
+    case low = 0.25
+    case medium = 0.5
+    case high = 0.75
+    case highest = 1
+}
+
+public enum ImageType: String {
+    case jpeg, png
 }
 
 extension UIImage {
 
-  private var isHeicSupported: Bool {
-    // swiftlint:disable force_cast
-      (CGImageDestinationCopyTypeIdentifiers() as! [String]).contains("public.heic")
-  }
+    private var isHeicSupported: Bool {
+        // swiftlint:disable force_cast
+        (CGImageDestinationCopyTypeIdentifiers() as! [String]).contains("public.heic")
+    }
 
-    public func compressImage(_ compressionQuality: CompressionQuality) -> (Data?, String) {
+    /**
+     Compresses an image to JPEG or PNG format.
 
-      if isHeicSupported, let heicData = heic(compressionQuality: compressionQuality) {
-          // write your heic image data to disk
-          return (heicData, "heic")
-      } else {
-          #if os(iOS)
-            guard let data = jpegData(compressionQuality: compressionQuality.rawValue) else {
-              return (nil, "")
+     - Parameter compressionQuality: The quality of the compressed image.
+     - Parameter imageType: The desired output image type.
+     - Returns: A tuple containing the compressed image data and its file extension, or throws an error if compression fails.
+     */
+    public func compressImage(compressionQuality: CompressionQuality, imageType: ImageType) throws -> (Data, String) {
+
+        let heicData = try heic(compressionQuality: compressionQuality)
+
+        if isHeicSupported {
+            return (heicData, "heic")
+        } else {
+            guard let jpegData = jpegData(compressionQuality: compressionQuality.rawValue) else {
+                throw ImageCompressionError.compressionFailed
             }
 
-            return (data, "jpeg")
+            switch imageType {
+            case .jpeg:
+                return (jpegData, imageType.rawValue)
 
-          #elseif os(OSX)
-          logger.debug("Value of type 'NSImage' has no member 'jpegData'")
-            fatalError("Value of type 'NSImage' has no member 'jpegData'")
-          #endif
+            case .png:
+                guard let image = UIImage(data: jpegData) else {
+                    throw ImageCompressionError.invalidImageData
+                }
+
+                if let pngData = image.pngData() {
+                    return (pngData, imageType.rawValue)
+                } else {
+                    throw ImageCompressionError.compressionFailed
+                }
+            }
+        }
     }
-  }
+
+
+
 }
 
 extension UIImage {
-  public enum HEICError: Error {
-    case heicNotSupported
-    case cgImageMissing
-    case couldNotFinalize
-  }
+    public enum HEICError: Error {
+        case heicNotSupported
+        case cgImageMissing
+        case couldNotFinalize
+    }
 }
 
 extension UIImage {
 
-    func heic(compressionQuality: CompressionQuality) -> Data? {
+    /**
+     Compresses the image to HEIC format.
+
+     - Parameter compressionQuality: The quality of the compressed image.
+     - Returns: The compressed HEIC image data, or throws an error if compression fails.
+     */
+    private func heic(compressionQuality: CompressionQuality) throws -> Data {
+
         guard
             let mutableData = CFDataCreateMutable(nil, 0),
             let destination = CGImageDestinationCreateWithData(mutableData, "public.heic" as CFString, 1, nil),
             let cgImage = cgImage
         else {
             logger.debug("heic compressionQuality mutableData, destination or cgImage nil")
-            return nil
+            throw HEICError.cgImageMissing
         }
 
         CGImageDestinationAddImage(
@@ -108,7 +143,7 @@ extension UIImage {
             CGImageDestinationFinalize(destination)
         else {
             logger.debug("CGImageDestinationFinalize nil")
-            return nil
+            throw HEICError.couldNotFinalize
         }
 
         return mutableData as Data
@@ -123,14 +158,14 @@ extension UIImage {
 extension CGImagePropertyOrientation {
     init(_ uiOrientation: UIImage.Orientation) {
         switch uiOrientation {
-            case .up: self = .up
-            case .upMirrored: self = .upMirrored
-            case .down: self = .down
-            case .downMirrored: self = .downMirrored
-            case .left: self = .left
-            case .leftMirrored: self = .leftMirrored
-            case .right: self = .right
-            case .rightMirrored: self = .rightMirrored
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
         @unknown default:
             logger.error("unknown default uiOrientation missing")
             fatalError()
